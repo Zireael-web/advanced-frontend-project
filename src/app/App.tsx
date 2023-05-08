4ee162369c8fc26addd563a938149500faad5616
Ivan <Ivan.Vasilenko.01234@mail.ru>
2023-05-08 23:14:57 +0300

    FSD Architecture - folders set

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes, Link } from 'react-router-dom';
 import { Suspense } from 'react';
 
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 import './styles/index.scss'
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 
 
@@ -21,8 +21,8 @@ const App = () => {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />}/>
-                    <Route path={'/'} element={<MainPageAsync />}/>
+                    <Route path={'/about'} element={<AboutPage />}/>
+                    <Route path={'/'} element={<MainPage />}/>
                 </Routes>
             </Suspense>
         </div>
