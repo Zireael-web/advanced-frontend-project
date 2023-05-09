dcab823575048e930edaad3279a806568c75b963
Ivan <Ivan.Vasilenko.01234@mail.ru>
2023-05-09 14:23:21 +0300

    router config, Navbar widget

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,8 @@
-import { Route, Routes, Link } from 'react-router-dom';
-import { Suspense } from 'react';
-
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-
 import './styles/index.scss'
 import { useTheme } from './providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 
 
@@ -16,15 +12,10 @@ const App = () => {
 
     return (    
         <div className={classNames('app', {}, [theme])}>
+            
+            <Navbar />
             <button onClick={toggleTheme}>TOGGLE THEME</button>
-            <Link to={'/'}>Главная</Link>   
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />}/>
-                    <Route path={'/'} element={<MainPage />}/>
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
