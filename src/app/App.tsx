a460bca5c10354e10402963e131c86e4d333a8d1
Ivan <Ivan.Vasilenko.01234@mail.ru>
2023-05-09 17:01:03 +0300

    sidebar widget

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { useTheme } from './providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 
 
 
@@ -14,7 +15,11 @@ const App = () => {
         <div className={classNames('app', {}, [theme])}>
             
             <Navbar />
-            <AppRouter />
+            <div className='content-page'>
+                <Sidebar />
+                <AppRouter />
+            </div>
+            
         </div>
     );
 };
