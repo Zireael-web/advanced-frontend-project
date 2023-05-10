342bd0d3fea53c723394d8b483f0917bd20209cb
Ivan <Ivan.Vasilenko.01234@mail.ru>
2023-05-10 23:32:04 +0300

    i18n added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,24 @@
-import './styles/index.scss'
 import { useTheme } from './providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
 
-
+import './styles/index.scss'
 
 const App = () => {
     const { theme } = useTheme();
 
-
     return (    
         <div className={classNames('app', {}, [theme])}>
-            
-            <Navbar />
-            <div className='content-page'>
-                <Sidebar />
-                <AppRouter />
-            </div>
-            
+            <Suspense fallback="">
+                <Navbar />
+                <div className='content-page'>
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
