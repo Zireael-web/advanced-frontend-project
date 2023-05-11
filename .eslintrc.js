79f87b0fb6dcb20c5784b9a891bc0322a4adaf79
Ivan <Ivan.Vasilenko.01234@mail.ru>
2023-05-11 23:44:41 +0300

    stylelint added

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "plugin:i18next/recommended"
     ],
     "overrides": [
     ],
@@ -17,7 +18,8 @@ module.exports = {
     },
     "plugins": [
         "react",
-        "@typescript-eslint"
+        "@typescript-eslint",
+        "i18next"
     ],
     "rules": {
         'react/jsx-indent': [2, 4],
@@ -35,6 +37,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
+        "i18next/no-literal-string": ["error", {markupOnly: true}]
     },
     globals: {
         __IS_DEV__: true,
